05635132a5a4a3963abeb8a2eaf80f4bf41e19e5
Huifeng <sgrrzhf@qq.com>
2018-12-01 19:05:32 +0800

    使用新版gnome-terminal -x 变为 --

--- Templates/auto_load.mk
+++ Templates/auto_load.mk
@@ -8,13 +8,13 @@ SPEED = 4000
 
 .PHONY: debug
 debug:
-	gnome-terminal --title "CGDB" -x \
+	gnome-terminal --title "CGDB" -- \
 	cgdb -d \
 	arm-none-eabi-gdb $$(BUILD_DIR)/$$(TARGET).elf
 
 .PHONY: jlinkgdbserver
 jlinkgdbserver:
-	gnome-terminal --title "JLinkGDBServer" -x \
+	gnome-terminal --title "JLinkGDBServer" -- \
 	JLinkGDBServer -device $$(DEVICE) -if $$(IF) -speed $$(SPEED)
 
 .PHONY: load
